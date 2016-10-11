7eb26fc47f423e9dd1b2e0f2086aae7139c7979a
kurian <kurianck.mail@gmail.com>
2016-10-11 14:21:46 +0530

    UPDATE
    observation:added a json from which to read

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -27,7 +27,7 @@ angular.module('myApp.controllers', []).
   }).
  controller("helloController",function($scope){
 	// items="\[\{\"text\":\"google,url:http://google.co.in\"\}\{\"text\":\"ebay\",\"url\":\"www.ebay.in\"\}\{\"text\":\"quora\",\"url\":\"www.quora.com\"\}\{\"text\":\"linkedin\",\"url\":\"http://linkedin.com\"\}:\]";
-	var items="\[\{\"text\":\"hello\",\"url\":\"http://localhost.com\"\}\]";
+	var items="\[\{\"text\":\"hello\",\"url\":\"http://localhost.com\"\},\{\"text\":\"google\",\"url\":\"http://google.co.in\"\},\{\"text\":\"linkedin\",\"url\":\"http://linkedin.com\"\},\{\"text\":\"ebay\",\"url\":\"http://ebay.com\"\},\{\"text\":\"amazon\",\"url\":\"http://amazon.com\"\},\{\"text\":\"library\",\"url\":\"http://bookzz.org\"\}\]";
 	 $scope.itemList=JSON.parse(items);
 //$scope.itemList=$http.get(https://api.twitter.com/1.1/search/tweets.json);
  });
